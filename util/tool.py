5cbe185c4b9d941362088f0451ff38ca2ce7d779
root <root@tf-1.novalocal>
2018-12-26 11:15:06 +0800

    conf

--- util/tool.py
+++ util/tool.py
@@ -12,42 +12,26 @@ def need_auth(func):
             handler._transforms = []
             handler.finish()
         def verify_auth(headers):
-            print 'get auth head'
-            print dir(headers)
-            print headers
             authInfo = headers.get('Authorization', None)
             if authInfo is None or not authInfo.startswith('Basic '):
                 return False
-            print authInfo
             authDecoded = base64.decodestring(authInfo[6:])
             username, passwd = authDecoded.split(":")
-            print username, passwd
             digestFromUser = hashlib.md5(passwd.encode('utf-8')).hexdigest()
-            print "digest from user: " + digestFromUser
             sql = "select password from users where name = '{0}'".format(username)
             res = MysqlHelper().execute(sql, True)
             if not res:
                 return False
             digestFromDb = res[0][0]
-            print "digest from db: " + digestFromDb
             handler.basicUsername = username
             return digestFromUser == digestFromDb
         handler = None
         if len(args) > 0 and isinstance(args[0], tornado.web.RequestHandler):
-            print 'yes, got handler cls'
             handler = args[0]
-            print '\n'
-            print '='*10
-            print handler.request.headers
-            print '='*10
-            print '\n'
             if not verify_auth(handler.request.headers):
-                print 'auth failed ...'
                 set_error(handler)
                 return False
         else:
-            print 'no handler cls'
             return False
-        print 'real func ......'
         return func(*args, **kwargs)
     return wrapper
